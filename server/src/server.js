855f8fa7dd1e238105bbe2b1962284bf9c847602
Vivek Sharma <viveksharma5970@gmail.com>
2025-08-18 13:24:30 +0530

    update

--- server/src/server.js
+++ server/src/server.js
@@ -30,9 +30,9 @@ app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "..client/dist")));
 
-    app.get("/*", (req, res)=> {
-      res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-    });
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
 }
 
 server.listen(5001, () => {
