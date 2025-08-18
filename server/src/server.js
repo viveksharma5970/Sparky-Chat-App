de279c811c5b20b6ce02843324927dcfb18f855d
Vivek Sharma <viveksharma5970@gmail.com>
2025-08-18 13:03:47 +0530

    update

--- server/src/server.js
+++ server/src/server.js
@@ -28,7 +28,11 @@ app.get("/", (req, res) => {
 });
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.use(express.static(path.join(__dirname, "..client/dist")));
+
+    app.get("*", (req, res)=> {
+      res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+    });
 }
 
 server.listen(5001, () => {
