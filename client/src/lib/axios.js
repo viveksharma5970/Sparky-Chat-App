9c7f913ca0ac02d015076d742612044380dec01d
Vivek Sharma <viveksharma5970@gmail.com>
2025-08-18 21:30:48 +0530

    updated backendlinks

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://sparky-chat-app-backend.onrender.com/api";
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
