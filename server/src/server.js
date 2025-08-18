7321cae3767b352c3da6e58f5a94803495651ef5
Vivek Sharma <viveksharma5970@gmail.com>
2025-08-18 21:34:00 +0530

    update

--- server/src/server.js
+++ server/src/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://sparky-chat-app.vercel.app"],
     credentials: true,
   })
 );
