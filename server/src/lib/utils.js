3325a041cb508e6de7292e4f12d0e0ef4a11eebb
viveksharma5970 <120032082+viveksharma5970@users.noreply.github.com>
2025-10-31 21:03:00 +0530

    Change cookie SameSite attribute to 'none'

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7*24*60*60*1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
         secure: process.env.NODE_ENV !== "development",
     });
 
